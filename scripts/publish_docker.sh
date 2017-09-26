9c2c8504deccc42ca037dc47fac8ceb580f0cace
Vlad Ungureanu <vladu@palantir.com>
2017-09-25 20:18:56 -0700

    Publish snapshot docker containers

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -3,5 +3,11 @@
 set -eu
 
 docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
-docker tag palantirtechnologies/bulldozer palantirtechnologies/bulldozer:$(./godelw project-version)
-docker push palantirtechnologies/bulldozer
+
+if git describe --exact-match --tags HEAD; then
+    docker tag palantirtechnologies/bulldozer palantirtechnologies/bulldozer:$(./godelw project-version)
+    docker push palantirtechnologies/bulldozer:$(./godelw project-version)
+else
+    docker tag palantirtechnologies/bulldozer palantirtechnologies/bulldozer:snapshot
+    docker push palantirtechnologies/bulldozer:snapshot
+fi
