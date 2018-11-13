7097d50c887ee7d6814b9dec2f7f2db565dbec69
Javier Campanini <jcampanini@palantir.com>
2018-11-13 10:55:13 -0500

    fmt

---
+++ server/handler/pull_request.go
@@ -0,0 +1,73 @@
+// Copyright 2018 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handler
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/google/go-github/github"
+	"github.com/palantir/go-githubapp/githubapp"
+	"github.com/pkg/errors"
+
+	"github.com/palantir/bulldozer/pull"
+)
+
+type PullRequest struct {
+	Base
+}
+
+func (h *PullRequest) Handles() []string {
+	return []string{"pull_request"}
+}
+
+func (h *PullRequest) Handle(ctx context.Context, eventType, deliveryID string, payload []byte) error {
+	var event github.PullRequestEvent
+	if err := json.Unmarshal(payload, &event); err != nil {
+		return errors.Wrap(err, "failed to parse pull request event payload")
+	}
+
+	repo := event.GetRepo()
+	owner := repo.GetOwner().GetLogin()
+	repoName := repo.GetName()
+	number := event.GetNumber()
+	installationID := githubapp.GetInstallationIDFromEvent(&event)
+	ctx, logger := githubapp.PreparePRContext(ctx, installationID, repo, number)
+
+	if event.GetAction() == "closed" {
+		logger.Debug().Msg("Doing nothing since pull request is closed")
+		return nil
+	}
+
+	client, err := h.ClientCreator.NewInstallationClient(installationID)
+	if err != nil {
+		return errors.Wrap(err, "failed to instantiate github client")
+	}
+
+	pr, _, err := client.PullRequests.Get(ctx, owner, repoName, number)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get pull request %s/%s#%d", owner, repoName, number)
+	}
+	pullCtx := pull.NewGithubContext(client, pr, owner, repoName, number)
+
+	if err := h.ProcessPullRequest(ctx, pullCtx, client, pr); err != nil {
+		logger.Error().Err(errors.WithStack(err)).Msg("Error processing pull request")
+	}
+
+	return nil
+}
+
+// type assertion
+var _ githubapp.EventHandler = &PullRequest{}
