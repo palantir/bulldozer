08efe6866fe81693f6da541cc4575a49444b656a
Andrew Svoboda <svoboda.andrew@gmail.com>
2019-04-15 09:16:01 +0100

    chore: Move to baseapp logging config (#109)
    * chore: Move to baseapp logging config
    
    * shim new LoggingConfig to prevent API breaks

--- vendor/github.com/palantir/go-baseapp/baseapp/config.go
+++ vendor/github.com/palantir/go-baseapp/baseapp/config.go
@@ -27,3 +27,12 @@ type HTTPConfig struct {
 	PublicURL string     `yaml:"public_url" json:"publicUrl"`
 	TLSConfig *TLSConfig `yaml:"tls_config" json:"tlsConfig"`
 }
+
+// LoggingConfig contains options for logging, such as log level and textual representation.
+// It is usually embedded in a larger configuration struct.
+type LoggingConfig struct {
+	Level string `yaml:"level" json:"level"`
+
+	// Pretty will make the output human-readable
+	Pretty bool `yaml:"pretty" json:"pretty"`
+}
