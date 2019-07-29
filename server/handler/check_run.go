bd7776884a302c06d065a27203731c553fa4355f
Cyril Lakech <1169286+clakech@users.noreply.github.com>
2019-07-29 20:53:04 +0200

    Use full PR to retrieve event info in check_run handler (#142)

--- server/handler/check_run.go
+++ server/handler/check_run.go
@@ -73,7 +73,7 @@ func (h *CheckRun) Handle(ctx context.Context, eventType, deliveryID string, pay
 		pullCtx := pull.NewGithubContext(client, fullPR)
 
 		logger := logger.With().Int(githubapp.LogKeyPRNum, pr.GetNumber()).Logger()
-		if err := h.ProcessPullRequest(logger.WithContext(ctx), pullCtx, client, pr); err != nil {
+		if err := h.ProcessPullRequest(logger.WithContext(ctx), pullCtx, client, fullPR); err != nil {
 			logger.Error().Err(errors.WithStack(err)).Msg("Error processing pull request")
 		}
 	}
