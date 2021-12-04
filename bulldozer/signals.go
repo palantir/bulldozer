125f3a26eb5323f1bf881698ff265204d9cf2da5
Derek Jobst <djobst@palantir.com>
2021-12-03 18:03:59 -0800

    Refactor MatchesAny into several smaller functions

--- bulldozer/signals.go
+++ bulldozer/signals.go
@@ -57,81 +57,233 @@ func (s *Signals) Enabled() bool {
 // in this description and indicates the behavior (trigger, ignore) this
 // set of signals is associated with.
 func (s *Signals) MatchesAny(ctx context.Context, pullCtx pull.Context, tag string) (bool, string, error) {
+	descriptions, matches, err := s.labelMatches(ctx, pullCtx, tag)
+	if err != nil {
+		return false, "", err
+	}
+
+	if len(matches) > 0 {
+		return true, descriptions[0], nil
+	}
+
+	descriptions, _, err = s.commentMatches(ctx, pullCtx, tag)
+	if err != nil {
+		return false, "", err
+	}
+
+	if len(matches) > 0 {
+		return true, descriptions[0], nil
+	}
+
+	descriptions, _, err = s.commentSubstringMatches(ctx, pullCtx, tag)
+	if err != nil {
+		return false, "", err
+	}
+
+	if len(matches) > 0 {
+		return true, descriptions[0], nil
+	}
+
+	descriptions, _, err = s.prBodyMatches(ctx, pullCtx, tag)
+	if err != nil {
+		return false, "", err
+	}
+
+	if len(matches) > 0 {
+		return true, descriptions[0], nil
+	}
+
+	descriptions, _, err = s.branchMatches(ctx, pullCtx, tag)
+	if err != nil {
+		return false, "", err
+	}
+
+	if len(matches) > 0 {
+		return true, descriptions[0], nil
+	}
+
+	descriptions, _, err = s.branchPatternMatches(ctx, pullCtx, tag)
+	if err != nil {
+		return false, "", err
+	}
+
+	if len(matches) > 0 {
+		return true, descriptions[0], nil
+	}
+
+	return false, fmt.Sprintf("pull request does not match the %s", tag), nil
+}
+
+// labelMatches determines which label signals match the given PR. It returns:
+// - A list of descriptions for each matched signal
+// - A list of the matched signals
+func (s *Signals) labelMatches(ctx context.Context, pullCtx pull.Context, tag string) ([]string, Signal, error) {
 	logger := zerolog.Ctx(ctx)
+	matches := Signal{}
+	descriptions := []string{}
 
 	labels, err := pullCtx.Labels(ctx)
 	if err != nil {
-		return false, "unable to list pull request labels", err
+		return descriptions, matches, errors.Wrap(err, "unable to list pull request labels")
 	}
 
 	if len(labels) == 0 {
 		logger.Debug().Msgf("No labels found to match against")
 	}
+
 	for _, signalLabel := range s.Labels {
 		for _, label := range labels {
 			if strings.EqualFold(signalLabel, label) {
-				return true, fmt.Sprintf("pull request has a %s label: %q", tag, signalLabel), nil
+				matches = append(matches, signalLabel)
+				descriptions = append(descriptions, fmt.Sprintf("pull request has a %s label: %q", tag, signalLabel))
 			}
 		}
 	}
 
+	return descriptions, matches, nil
+}
+
+// commentMatches determines which comment signals match the given PR. It returns:
+// - A list of descriptions for each matched signal
+// - A list of the matched signals
+func (s *Signals) commentMatches(ctx context.Context, pullCtx pull.Context, tag string) ([]string, Signal, error) {
+	logger := zerolog.Ctx(ctx)
+	matches := Signal{}
+	descriptions := []string{}
+
 	body := pullCtx.Body()
 	comments, err := pullCtx.Comments(ctx)
 	if err != nil {
-		return false, "unable to list pull request comments", err
+		return descriptions, matches, errors.Wrap(err, "unable to list pull request comments")
 	}
 
 	if len(comments) == 0 {
 		logger.Debug().Msgf("No comments found to match against")
 	}
+
 	for _, signalComment := range s.Comments {
 		if body == signalComment {
-			return true, fmt.Sprintf("pull request body is a %s comment: %q", tag, signalComment), nil
-		}
-		for _, comment := range comments {
-			if comment == signalComment {
-				return true, fmt.Sprintf("pull request has a %s comment: %q", tag, signalComment), nil
+			matches = append(matches, signalComment)
+			descriptions = append(descriptions, fmt.Sprintf("pull request body is a %s comment: %q", tag, signalComment))
+		} else {
+			// TODO: Use a set and remove the else
+			for _, comment := range comments {
+				if comment == signalComment {
+					matches = append(matches, signalComment)
+					descriptions = append(descriptions, fmt.Sprintf("pull request has a %s comment: %q", tag, signalComment))
+				}
 			}
 		}
+
+	}
+
+	return descriptions, matches, nil
+}
+
+// commentSubstringMatches determines which comment substring signals match the given PR. It returns:
+// - A list of descriptions for each matched signal
+// - A list of the matched signals
+func (s *Signals) commentSubstringMatches(ctx context.Context, pullCtx pull.Context, tag string) ([]string, Signal, error) {
+	logger := zerolog.Ctx(ctx)
+	matches := Signal{}
+	descriptions := []string{}
+
+	body := pullCtx.Body()
+	comments, err := pullCtx.Comments(ctx)
+	if err != nil {
+		return descriptions, matches, errors.Wrap(err, "unable to list pull request comments")
 	}
 
 	if len(s.CommentSubstrings) == 0 {
 		logger.Debug().Msgf("No comment substrings found to match against")
 	}
+
 	for _, signalSubstring := range s.CommentSubstrings {
 		if strings.Contains(body, signalSubstring) {
-			return true, fmt.Sprintf("pull request body matches a %s substring: %q", tag, signalSubstring), nil
-		}
-		for _, comment := range comments {
-			if strings.Contains(comment, signalSubstring) {
-				return true, fmt.Sprintf("pull request comment matches a %s substring: %q", tag, signalSubstring), nil
+			matches = append(matches, signalSubstring)
+			descriptions = append(descriptions, fmt.Sprintf("pull request body matches a %s substring: %q", tag, signalSubstring))
+		} else {
+			// TODO: Use a set and remove the else
+			for _, comment := range comments {
+				if strings.Contains(comment, signalSubstring) {
+					matches = append(matches, signalSubstring)
+					descriptions = append(descriptions, fmt.Sprintf("pull request comment matches a %s substring: %q", tag, signalSubstring))
+				}
 			}
 		}
 	}
 
+	return descriptions, matches, nil
+}
+
+// prBodyMatches determines which PR body signals match the given PR. It returns:
+// - A list of descriptions for each matched signal
+// - A list of the matched signals
+func (s *Signals) prBodyMatches(ctx context.Context, pullCtx pull.Context, tag string) ([]string, Signal, error) {
+	logger := zerolog.Ctx(ctx)
+	matches := Signal{}
+	descriptions := []string{}
+
+	body := pullCtx.Body()
+
 	if len(s.PRBodySubstrings) == 0 {
 		logger.Debug().Msgf("No PR body substrings found to match against")
 	}
+
 	for _, signalSubstring := range s.PRBodySubstrings {
 		if strings.Contains(body, signalSubstring) {
-			return true, fmt.Sprintf("pull request body matches a %s substring: %q", tag, signalSubstring), nil
+			matches = append(matches, signalSubstring)
+			descriptions = append(descriptions, fmt.Sprintf("pull request body matches a %s substring: %q", tag, signalSubstring))
 		}
 	}
+
+	return descriptions, matches, nil
+}
+
+// branchMatches determines which branch signals match the given PR. It returns:
+// - A list of descriptions for each matched signal
+// - A list of the matched signals
+func (s *Signals) branchMatches(ctx context.Context, pullCtx pull.Context, tag string) ([]string, Signal, error) {
+	logger := zerolog.Ctx(ctx)
+	matches := Signal{}
+	descriptions := []string{}
 
 	targetBranch, _ := pullCtx.Branches()
-	if len(s.Branches) == 0 || len(s.BranchPatterns) == 0 {
-		logger.Debug().Msgf("No branches or branch patterns found to match against")
+
+	if len(s.Branches) == 0 {
+		logger.Debug().Msgf("No branches found to match against")
 	}
+
 	for _, signalBranch := range s.Branches {
 		if targetBranch == signalBranch {
-			return true, fmt.Sprintf("pull request target is a %s branch: %q", tag, signalBranch), nil
-		}
-	}
-	for _, signalBranch := range s.BranchPatterns {
-		if matched, _ := regexp.MatchString(fmt.Sprintf("^%s$", signalBranch), targetBranch); matched {
-			return true, fmt.Sprintf("pull request target branch (%q) matches pattern: %q", targetBranch, signalBranch), nil
+			matches = append(matches, signalBranch)
+			descriptions = append(descriptions, fmt.Sprintf("pull request target is a %s branch: %q", tag, signalBranch))
 		}
 	}
 
-	return false, fmt.Sprintf("pull request does not match the %s", tag), nil
+	return descriptions, matches, nil
+}
+
+// branchPatternMatches determines which branch pattern signals match the given PR. It returns:
+// - A list of descriptions for each matched signal
+// - A list of the matched signals
+func (s *Signals) branchPatternMatches(ctx context.Context, pullCtx pull.Context, tag string) ([]string, Signal, error) {
+	logger := zerolog.Ctx(ctx)
+	matches := Signal{}
+	descriptions := []string{}
+
+	targetBranch, _ := pullCtx.Branches()
+
+	if len(s.BranchPatterns) == 0 {
+		logger.Debug().Msgf("No branch patterns found to match against")
+	}
+
+	for _, signalBranch := range s.BranchPatterns {
+		if matched, _ := regexp.MatchString(fmt.Sprintf("^%s$", signalBranch), targetBranch); matched {
+			matches = append(matches, signalBranch)
+			descriptions = append(descriptions, fmt.Sprintf("pull request target branch (%q) matches pattern: %q", targetBranch, signalBranch))
+		}
+	}
+
+	return descriptions, matches, nil
 }
