70be4f9e70a8e0d8112ab44f54bac902444e6022
Derek Jobst <djobst@palantir.com>
2022-02-10 12:28:57 -0800

    Capitalize Received

--- server/handler/pull_request_review.go
+++ server/handler/pull_request_review.go
@@ -45,7 +45,7 @@ func (h *PullRequestReview) Handle(ctx context.Context, eventType, deliveryID st
 	installationID := githubapp.GetInstallationIDFromEvent(&event)
 	ctx, logger := githubapp.PreparePRContext(ctx, installationID, repo, number)
 
-	logger.Debug().Msgf("received pull_request_review %s event", event.GetAction())
+	logger.Debug().Msgf("Received pull_request_review %s event", event.GetAction())
 
 	client, err := h.ClientCreator.NewInstallationClient(installationID)
 	if err != nil {
