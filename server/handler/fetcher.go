f2e8a7d9cef010cb2e6c63b652b25cc552a1332e
asvoboda <svoboda.andrew@gmail.com>
2022-10-05 12:36:01 +0100

    Update go-github to v47

--- server/handler/fetcher.go
+++ server/handler/fetcher.go
@@ -17,7 +17,7 @@ package handler
 import (
 	"context"
 
-	"github.com/google/go-github/v45/github"
+	"github.com/google/go-github/v47/github"
 	"github.com/palantir/bulldozer/bulldozer"
 	"github.com/palantir/go-githubapp/appconfig"
 	"github.com/rs/zerolog"
