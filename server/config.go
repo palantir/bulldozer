97254f518eae4fc199cd62fca0ae0c5bd842e03a
Depickere Sven <sven.depickere@hotmail.com>
2022-11-28 21:50:17 +0100

    Options can't be configured as ENV var (#359)

--- server/config.go
+++ server/config.go
@@ -18,7 +18,7 @@ import (
 	"os"
 
 	"github.com/c2h5oh/datasize"
-	"github.com/palantir/bulldozer/bulldozer"
+	"github.com/palantir/bulldozer/server/handler"
 	"github.com/palantir/go-baseapp/baseapp"
 	"github.com/palantir/go-baseapp/baseapp/datadog"
 	"github.com/palantir/go-githubapp/githubapp"
@@ -27,15 +27,13 @@ import (
 )
 
 const (
-	DefaultEnvPrefix               = "BULLDOZER_"
-	DefaultSharedRepository        = ".github"
-	DefaultSharedConfigurationPath = "bulldozer.yml"
+	DefaultEnvPrefix = "BULLDOZER_"
 )
 
 type Config struct {
 	Server  baseapp.HTTPConfig `yaml:"server"`
 	Github  githubapp.Config   `yaml:"github"`
-	Options Options            `yaml:"options"`
+	Options handler.Options    `yaml:"options"`
 	Logging LoggingConfig      `yaml:"logging"`
 	Datadog datadog.Config     `yaml:"datadog"`
 	Cache   CacheConfig        `yaml:"cache"`
@@ -56,20 +54,6 @@ type WorkerConfig struct {
 	QueueSize int `yaml:"queue_size"`
 }
 
-type Options struct {
-	AppName                  string `yaml:"app_name"`
-	PushRestrictionUserToken string `yaml:"push_restriction_user_token"`
-
-	ConfigurationPath       string            `yaml:"configuration_path"`
-	SharedRepository        string            `yaml:"shared_repository"`
-	SharedConfigurationPath string            `yaml:"shared_configuration_path"`
-	DefaultRepositoryConfig *bulldozer.Config `yaml:"default_repository_config"`
-
-	ConfigurationV0Paths []string `yaml:"configuration_v0_paths"`
-
-	DisableUpdateFeature bool `yaml:"disable_update_feature"`
-}
-
 func ParseConfig(bytes []byte) (*Config, error) {
 	var c Config
 	if err := yaml.UnmarshalStrict(bytes, &c); err != nil {
@@ -84,16 +68,7 @@ func ParseConfig(bytes []byte) (*Config, error) {
 	}
 	c.Server.SetValuesFromEnv(envPrefix)
 
-	if v, ok := os.LookupEnv(envPrefix + "PUSH_RESTRICTION_USER_TOKEN"); ok {
-		c.Options.PushRestrictionUserToken = v
-	}
-
-	if c.Options.SharedRepository == "" {
-		c.Options.SharedRepository = DefaultSharedRepository
-	}
-	if c.Options.SharedConfigurationPath == "" {
-		c.Options.SharedConfigurationPath = DefaultSharedConfigurationPath
-	}
+	c.Options.SetValuesFromEnv(envPrefix + "OPTIONS_")
 
 	return &c, nil
 }
---
+++ server/handler/options.go
@@ -0,0 +1,84 @@
+// Copyright 2022 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handler
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/palantir/bulldozer/bulldozer"
+)
+
+const (
+	DefaultSharedRepository        = ".github"
+	DefaultConfigurationPath       = ".bulldozer.yml"
+	DefaultSharedConfigurationPath = "bulldozer.yml"
+	DefaultAppName                 = "bulldozer"
+)
+
+type Options struct {
+	AppName                  string `yaml:"app_name"`
+	PushRestrictionUserToken string `yaml:"push_restriction_user_token"`
+
+	ConfigurationPath       string            `yaml:"configuration_path"`
+	SharedRepository        string            `yaml:"shared_repository"`
+	SharedConfigurationPath string            `yaml:"shared_configuration_path"`
+	DefaultRepositoryConfig *bulldozer.Config `yaml:"default_repository_config"`
+
+	ConfigurationV0Paths []string `yaml:"configuration_v0_paths"`
+
+	DisableUpdateFeature bool `yaml:"disable_update_feature"`
+}
+
+func (o *Options) fillDefaults() {
+	if o.ConfigurationPath == "" {
+		o.ConfigurationPath = DefaultConfigurationPath
+	}
+	if o.AppName == "" {
+		o.AppName = DefaultAppName
+	}
+	if o.SharedRepository == "" {
+		o.SharedRepository = DefaultSharedRepository
+	}
+	if o.SharedConfigurationPath == "" {
+		o.SharedConfigurationPath = DefaultSharedConfigurationPath
+	}
+}
+
+func (o *Options) SetValuesFromEnv(prefix string) {
+	setStringFromEnv("CONFIGURATION_PATH", prefix, &o.ConfigurationPath)
+	setStringFromEnv("APP_NAME", prefix, &o.AppName)
+	setStringFromEnv("SHARED_REPOSITORY", prefix, &o.SharedRepository)
+	setStringFromEnv("SHARED_CONFIGURATION_PATH", prefix, &o.SharedConfigurationPath)
+	setBooleanFromEnv("DISABLE_UPDATE_FEATURE", prefix, &o.DisableUpdateFeature)
+	setStringFromEnv("PUSH_RESTRICTION_USER_TOKEN", prefix, &o.PushRestrictionUserToken)
+	o.fillDefaults()
+}
+
+func setStringFromEnv(key, prefix string, value *string) bool {
+	if v, ok := os.LookupEnv(prefix + key); ok {
+		*value = v
+		return true
+	}
+	return false
+}
+
+func setBooleanFromEnv(key, prefix string, value *bool) bool {
+	if v, ok := os.LookupEnv(prefix + key); ok {
+		*value, _ = strconv.ParseBool(v)
+		return true
+	}
+	return false
+}
