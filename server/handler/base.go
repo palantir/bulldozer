e4f5d98a98e5b661253689eb51d487cc198a3cd6
asvoboda <svoboda.andrew@gmail.com>
2023-09-11 11:01:50 +0100

    Update go-github to v55

--- server/handler/base.go
+++ server/handler/base.go
@@ -17,7 +17,7 @@ package handler
 import (
 	"context"
 
-	"github.com/google/go-github/v53/github"
+	"github.com/google/go-github/v55/github"
 	"github.com/palantir/bulldozer/bulldozer"
 	"github.com/palantir/bulldozer/pull"
 	"github.com/palantir/go-githubapp/githubapp"
