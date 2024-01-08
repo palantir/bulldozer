bbff21eb2ce241d10e54cdd01b27a4bd811d08b5
asvoboda <svoboda.andrew@gmail.com>
2024-01-08 13:12:31 +0000

    Update go-github to v57

--- bulldozer/merge.go
+++ bulldozer/merge.go
@@ -22,7 +22,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v56/github"
+	"github.com/google/go-github/v57/github"
 	"github.com/palantir/bulldozer/pull"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
