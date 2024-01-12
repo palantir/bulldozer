0e95886a51bd608c6aeca5adf2ec46c4639e2393
asvoboda <svoboda.andrew@gmail.com>
2024-01-12 11:16:09 +0000

    Update go-github to v58

--- server/handler/status.go
+++ server/handler/status.go
@@ -18,7 +18,7 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/google/go-github/v57/github"
+	"github.com/google/go-github/v58/github"
 	"github.com/palantir/bulldozer/pull"
 	"github.com/palantir/go-githubapp/githubapp"
 	"github.com/pkg/errors"
