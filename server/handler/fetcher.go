e0fe11d6c34d5b9225451d4f45b088b724d1167c
Billy Keyes <bkeyes@palantir.com>
2025-05-01 16:20:14 -0700

    Update go-github to v71

--- server/handler/fetcher.go
+++ server/handler/fetcher.go
@@ -17,7 +17,7 @@ package handler
 import (
 	"context"
 
-	"github.com/google/go-github/v70/github"
+	"github.com/google/go-github/v71/github"
 	"github.com/palantir/bulldozer/bulldozer"
 	"github.com/palantir/go-githubapp/appconfig"
 	"github.com/rs/zerolog"
